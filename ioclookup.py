590ff9c10559f7c34cbfad4276cc659202cfff1b
dargen3 <dargen3@centrum.cz>
2018-04-25 09:31:40 +0200

    all ioc

--- ioclookup.py
+++ ioclookup.py
@@ -22,30 +22,8 @@ def elasticsearch_ioc():
     if environ["DEBUG"]:
         with open("hits.txt", "w") as file:
             file.write(str(hits))
-
-
-    ioc_data_dict = {}
-    for ioc in hits["hits"]["hits"]:
-        if "accuracy" in ioc["_source"]:
-            acc_data = ioc["_source"]["accuracy"]
-            if type(acc_data) is dict:
-                accuracy = 0
-                for key in acc_data:
-                    accuracy += acc_data[key]
-                if accuracy in ioc_data_dict:
-                    date_writed = dt.strptime(ioc_data_dict[accuracy]["_source"]["time"]["observation"][:10], "%Y-%m-%d")
-                    date_actual = dt.strptime(ioc["_source"]["time"]["observation"][:10], "%Y-%m-%d")
-                    if ((date_writed - date_actual).total_seconds()) > 0:
-                        continue
-                ioc_data_dict[accuracy] = ioc["_source"]
-    if not len(ioc_data_dict):
-        data = {}
-    else:
-        data = ioc_data_dict[max(ioc_data_dict)]
-
-
-
-    response = app.response_class(response=dumps(data), status=200, mimetype='application/json')
+    ioc_data = {[ioc["_source"] for ioc in hits["hits"]["hits"]]}
+    response = app.response_class(response=dumps(ioc_data), status=200, mimetype='application/json')
     return response
 
 if __name__ == '__main__':
